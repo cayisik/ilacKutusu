4343ae0f716d522872dc837eb49ae778322ee049
Can Ayışık <canayisik@outlook.com>
2020-08-08 14:15:42 +0300

    Initial Commit

---
+++ Ilac Kutusu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ilac Kutusu
+//
+//  Created by Can Ayışık on 8.08.2020.
+//  Copyright © 2020 Can Ayışık. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
